f31535381e4773b84814b9ddfa35b328153d9b00
balatuluva <bala.tulva@gmail.com>
2023-05-01 11:30:42 +0100

    security group

--- servers.tf
+++ servers.tf
@@ -4,6 +4,10 @@ data "aws_ami" "centos" {
   name_regex  = "Centos-8-DevOps-Practice"
 }
 
+data "aws_security_group" "selected" {
+  name = "allow-all"
+}
+
 resource "aws_instance" "frontend" {
   ami              = data.aws_ami.centos.image_id
   instance_type    = "t3.micro"
