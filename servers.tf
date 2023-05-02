8f3ad76794dc11fbbe44afe76a6fcab1ff6ea3e8
balatuluva <bala.tulva@gmail.com>
2023-05-02 07:10:21 +0100

    loops in servers

--- servers.tf
+++ servers.tf
@@ -12,101 +12,106 @@ variable "instance_type" {
   default = "t3.small"
 }
 
+variable "components" {
+  default = ["frontend", "mongodb", "redis"]
+}
+
 resource "aws_instance" "frontend" {
+  count            = length(var.components)
   ami              = data.aws_ami.centos.image_id
   instance_type    = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = "frontend"
+    Name = var.components[count.index]
   }
 }
 
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z086778943HEZIHKI7U9"
-  name    = "frontend.gehana26.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
+#resource "aws_route53_record" "frontend" {
+#  zone_id = "Z086778943HEZIHKI7U9"
+#  name    = "frontend.gehana26.online"
+#  type    = "A"
+#  ttl     = 30
+#  records = [aws_instance.frontend.private_ip]
+#}
 
-resource "aws_instance" "mongodb" {
-  ami              = data.aws_ami.centos.image_id
-  instance_type    = "t3.micro"
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_instance" "catalogue" {
-  ami              = data.aws_ami.centos.image_id
-  instance_type    = "t3.micro"
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_instance" "redis" {
-  ami              = data.aws_ami.centos.image_id
-  instance_type    = "t3.micro"
-
-  tags = {
-    Name = "redis"
-  }
-}
-
-resource "aws_instance" "user" {
-  ami              = data.aws_ami.centos.image_id
-  instance_type    = "t3.micro"
-
-  tags = {
-    Name = "user"
-  }
-}
-
-resource "aws_instance" "cart" {
-  ami              = data.aws_ami.centos.image_id
-  instance_type    = "t3.micro"
-
-  tags = {
-    Name = "cart"
-  }
-}
-
-resource "aws_instance" "mysql" {
-  ami              = data.aws_ami.centos.image_id
-  instance_type    = "t3.micro"
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_instance" "shipping" {
-  ami              = data.aws_ami.centos.image_id
-  instance_type    = "t3.micro"
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami              = data.aws_ami.centos.image_id
-  instance_type    = "t3.micro"
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-resource "aws_instance" "payment" {
-  ami              = data.aws_ami.centos.image_id
-  instance_type    = "t3.micro"
-
-  tags = {
-    Name = "payment"
-  }
-}
+#resource "aws_instance" "mongodb" {
+#  ami              = data.aws_ami.centos.image_id
+#  instance_type    = "t3.micro"
+#
+#  tags = {
+#    Name = "mongodb"
+#  }
+#}
+#
+#resource "aws_instance" "catalogue" {
+#  ami              = data.aws_ami.centos.image_id
+#  instance_type    = "t3.micro"
+#
+#  tags = {
+#    Name = "catalogue"
+#  }
+#}
+#
+#resource "aws_instance" "redis" {
+#  ami              = data.aws_ami.centos.image_id
+#  instance_type    = "t3.micro"
+#
+#  tags = {
+#    Name = "redis"
+#  }
+#}
+#
+#resource "aws_instance" "user" {
+#  ami              = data.aws_ami.centos.image_id
+#  instance_type    = "t3.micro"
+#
+#  tags = {
+#    Name = "user"
+#  }
+#}
+#
+#resource "aws_instance" "cart" {
+#  ami              = data.aws_ami.centos.image_id
+#  instance_type    = "t3.micro"
+#
+#  tags = {
+#    Name = "cart"
+#  }
+#}
+#
+#resource "aws_instance" "mysql" {
+#  ami              = data.aws_ami.centos.image_id
+#  instance_type    = "t3.micro"
+#
+#  tags = {
+#    Name = "mysql"
+#  }
+#}
+#
+#resource "aws_instance" "shipping" {
+#  ami              = data.aws_ami.centos.image_id
+#  instance_type    = "t3.micro"
+#
+#  tags = {
+#    Name = "shipping"
+#  }
+#}
+#
+#resource "aws_instance" "rabbitmq" {
+#  ami              = data.aws_ami.centos.image_id
+#  instance_type    = "t3.micro"
+#
+#  tags = {
+#    Name = "rabbitmq"
+#  }
+#}
+#
+#resource "aws_instance" "payment" {
+#  ami              = data.aws_ami.centos.image_id
+#  instance_type    = "t3.micro"
+#
+#  tags = {
+#    Name = "payment"
+#  }
+#}
