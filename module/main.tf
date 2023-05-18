659ae4b1f1588db106c065533ec62b1edf4a02c4
balatuluva <bala.tulva@gmail.com>
2023-05-18 20:13:32 +0100

    provisioner error remote

--- module/main.tf
+++ module/main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "instance" {
 resource "null_resource" "provisioner" {
   count = var.provisioner ? 1 : 0
   depends_on = [aws_instance.instance, aws_route53_record.records]
-  provisioner "local-exec" {
+  provisioner "remote-exec" {
     connection {
       type = "ssh"
       user = "centos"
