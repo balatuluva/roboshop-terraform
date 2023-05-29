2a442b68678833266026e6e61ca724f3b822884f
balatuluva <bala.tulva@gmail.com>
2023-05-29 13:11:08 +0100

    iam role policy correction

--- module/main.tf
+++ module/main.tf
@@ -55,8 +55,9 @@ resource "aws_iam_instance_profile" "instance_profile" {
   role = aws_iam_role.role.name
 }
 
-resource "aws_iam_policy" "ssm-ps-policy" {
+resource "aws_iam_role_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
+  role = aws_iam_role.role.id
   policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
